ec44d145107bfb274a53583c75abc4bca7e68d35
Farooq, Salman <salmanfarooq@my.unt.edu>
2025-07-18 09:31:00 -0500

    fixing repo name in package.json

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/site" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
     </Router>
